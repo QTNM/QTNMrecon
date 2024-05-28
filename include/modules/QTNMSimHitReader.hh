0ff334a5ee43881808464e2ddbcdf56b9e2b4751
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2024-05-28 15:41:58 +0100

    [code] update hit reader to working pattern.

--- include/modules/QTNMSimHitReader.hh
+++ include/modules/QTNMSimHitReader.hh
@@ -6,7 +6,6 @@
 #include <string>
 
 // ROOT
-#include "TFile.h"
 #include "TTreeReader.h"
 
 // must have include for pipeline
@@ -15,9 +14,8 @@
 class QTNMSimHitReader
 {
     public:
-        QTNMSimHitReader(std::string fname, std::string outbox); // constructor; required
+        QTNMSimHitReader(TTreeReader& re, std::string outbox); // constructor; required
         // input file name and new Key outbox label.
-        ~QTNMSimHitReader(); // destructor to close the file
 
         Event_map<std::any> operator()(); // this is called by the pipeline
 
@@ -30,8 +28,7 @@ class QTNMSimHitReader
     int evcounter;
 
     // ROOT file access for member functions
-    TFile* fin = nullptr;
-    TTreeReader* reader = nullptr;
+    TTreeReader& reader;
 
     // these below serve as string keys to access (read/write) the Event map
     std::string outkey;
--- modules/QTNMSimHitReader.cpp
+++ modules/QTNMSimHitReader.cpp
@@ -10,51 +10,41 @@
 // ROOT
 #include "TTreeReaderValue.h"
 
-QTNMSimHitReader::QTNMSimHitReader(std::string fname, std::string out) : 
+QTNMSimHitReader::QTNMSimHitReader(TTreeReader& re, std::string out) : 
     outkey(std::move(out)),
     maxEventNumber(-1), // default -1 for a all events
-    evcounter(0)
+    evcounter(0),
+    reader(re)
 {
-    // prepare the input file, read-only
-    try {
-        fin = new TFile(fname.data(),"READ");
-    }
-    catch (const std::exception& e) {
-        std::cout << "file open error. " << e.what() << std::endl;
-    }
-    reader = new TTreeReader("ntuple/Score", fin); // takes pointer to TFile
-    // reads QTNMSim output file with given ttree name
-}
-
-QTNMSimHitReader::~QTNMSimHitReader()
-{
-    fin->Close();
 }
 
 Event_map<std::any> QTNMSimHitReader::operator()()
 {
     // catch event number limit in pipeline, -1 = all, default
-    if (evcounter >= maxEventNumber && maxEventNumber > 0) // max has been set
-        throw yap::GeneratorExit{};
-    evcounter++;
+  if ((evcounter > maxEventNumber &&   // reached maximum event number
+       maxEventNumber > 0) ||          // max has been set
+      evcounter >= reader.GetEntries()) // reached end of file
+    
+    throw yap::GeneratorExit{};
+  evcounter++;
 
     Event_map<std::any> eventmap; // data item for delivery
     Event<std::any> outdata; // to hold all the data items from file
 
     // collect all Signal info from file, reader holds event iterator
-    TTreeReaderValue<int> eventID(*reader, "EventID"); // needs reader by reference
-    TTreeReaderValue<int> trackID(*reader, "TrackID");
-    TTreeReaderValue<double> edep(*reader, "Edep"); // interaction data
-    TTreeReaderValue<double> tstamp(*reader, "TimeStamp");
-    TTreeReaderValue<double> prek(*reader, "PreKine");
-    TTreeReaderValue<double> postk(*reader, "PostKine");
-    TTreeReaderValue<double> preth(*reader, "PreTheta");
-    TTreeReaderValue<double> postth(*reader, "PostTheta");
-    TTreeReaderValue<double> posx(*reader, "Posx"); // interaction location
-    TTreeReaderValue<double> posy(*reader, "Posy");
-    TTreeReaderValue<double> posz(*reader, "Posz");
+    TTreeReaderValue<int> eventID(reader, "EventID"); // needs reader by reference
+    TTreeReaderValue<int> trackID(reader, "TrackID");
+    TTreeReaderValue<double> edep(reader, "Edep"); // interaction data
+    TTreeReaderValue<double> tstamp(reader, "TimeStamp");
+    TTreeReaderValue<double> prek(reader, "PreKine");
+    TTreeReaderValue<double> postk(reader, "PostKine");
+    TTreeReaderValue<double> preth(reader, "PreTheta");
+    TTreeReaderValue<double> postth(reader, "PostTheta");
+    TTreeReaderValue<double> posx(reader, "Posx"); // interaction location
+    TTreeReaderValue<double> posy(reader, "Posy");
+    TTreeReaderValue<double> posz(reader, "Posz");
     
-    if (reader->Next()) {; // variables filled from file
+    if (reader.Next()) {; // variables filled from file
         outdata["eventID"] = std::any(*eventID); // de-reference an int to std::any
         outdata["trackID"] = std::any(*trackID);
         outdata["Edep"] = std::any(*edep);
