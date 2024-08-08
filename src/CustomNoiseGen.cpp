238233ec5a2d6947ff4a822a4eca665c06b87695
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2024-08-08 11:16:04 +0100

    [add] custom noise tool add repair.

--- src/CustomNoiseGen.cpp
+++ src/CustomNoiseGen.cpp
@@ -108,6 +108,10 @@ waveform_t CustomNoiseGenerator::add(waveform_t& sig, size_t pos)
 
     // add other to existing tsig at position pos, 
     // up to at most end of tsig, irrespective of the length of other
-    transform(tsig.begin()+pos,tsig.end(),sig.begin(),tsig.begin()+pos,std::plus<waveform_value>());
+    size_t j = 0;
+    for (size_t i=pos;i<tsig.size() && j<sig.size();++i) {
+      tsig[i] += sig[j];
+      ++j;
+    }
     return tsig;
 }
