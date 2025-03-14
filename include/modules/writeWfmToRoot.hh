34fc0dd98b13914dee339e121ba9f0323b39a6f5
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2025-03-14 16:54:15 +0000

    [writer] new writer module for pure waveform writing and truth data up to that point in reconstruction.

---
+++ include/modules/writeWfmToRoot.hh
@@ -0,0 +1,60 @@
+// ROOT waveform writer for a pipeline module
+
+#ifndef writerWfm_HH
+#define writerWfm_HH 1
+
+// std includes
+#include <string>
+#include <vector>
+#include <array>
+
+// ROOT
+#include "TTree.h"
+
+// must have include for pipeline
+#include <Event.hh>
+
+class WriterWfmToRoot
+{
+    public:
+        WriterWfmToRoot(TTree* tr, int na); // constructor; required
+        // Need antenna number as input for setting up output file
+        // That truth data is in DataPack but not available at construction.
+        // Configures the module. Could have more config parameters
+        // Minimum required are the key labels for input and output 
+        // of Event Map data item.
+
+        void operator()(DataPack dp); // this is called by the pipeline
+        // Writers/Sinks only receive a DataPack and return void as signature.
+
+        // getter/setter methods for configuration could live here.
+
+    private:
+    // include any configuration data members for internal use here.
+      int nantenna; // needed for construction of output file
+      TTree* mytree;
+      std::vector<vec_t*> purewave; // no unit storage in ROOT file
+      // doubles/int for values without unit
+      int nant, evID, trID;
+      double samplingtime; // from quantity<ns>
+      double avomega; // quantity<Hz>
+      double beatf; // quantity<Hz>
+      double chirprate; // quantity<Hz/s>
+      double bfield; // quantity<T>
+      // vertex
+      double posx; // quantity<m>
+      double posy; // quantity<m>
+      double posz; // quantity<m>
+      double kEnergy; // quantity<eV>
+      double pangle; // quantity<deg>
+      // hit data
+      std::vector<int>* hitevID;
+      std::vector<int>* hittrID;
+      std::vector<double>* hitx;
+      std::vector<double>* hity;
+      std::vector<double>* hitz;
+      std::vector<double>* hitedep;
+      std::vector<double>* hittime;
+      std::vector<double>* hitposttheta;
+};
+#endif
---
+++ modules/writeWfmToRoot.cpp
@@ -0,0 +1,124 @@
+// Root waveform Writer module implementation
+// writing a single value; expand for specific outputs.
+
+// std
+#include <iostream>
+#include <algorithm>
+#include <memory>
+
+// us
+#include "writeWfmToRoot.hh"
+#include "types.hh"
+
+WriterWfmToRoot::WriterWfmToRoot(TTree* tr, int na) : 
+  mytree(tr),
+  nantenna(na)
+{
+  // N antennae, one for each waveform; need to know at construction for writing
+  // construct scopedata entries
+  for (int i=0;i<nantenna;++i) {
+    vec_t* p;
+    purewave.push_back(p); // vector in purewave initialized
+  }
+  // can now point branch at dummy addresses; makes header only
+  mytree->Branch("truth_nantenna",&nant,"truth_nantenna/I");
+  mytree->Branch("truth_samplingtime_s",&samplingtime,"truth_samplingtime/D");
+  mytree->Branch("truth_avomega_Hz",&avomega,"truth_avomega/D");
+  mytree->Branch("truth_beatf_Hz",&beatf,"truth_beatf/D");
+  mytree->Branch("truth_chirp_Hz_s",&chirprate,"truth_chirp_rate/D");
+  mytree->Branch("truth_bfield_T",&bfield,"truth_bfield/D");
+  mytree->Branch("vertex_evID",&evID,"vertex_evID/I");
+  mytree->Branch("vertex_trackID",&trID,"vertex_trackID/I");
+  mytree->Branch("vertex_posx_m",&posx,"vertex_posx/D");
+  mytree->Branch("vertex_posy_m",&posy,"vertex_posy/D");
+  mytree->Branch("vertex_posz_m",&posz,"vertex_posz/D");
+  mytree->Branch("vertex_kinenergy_eV",&kEnergy,"vertex_kinenergy/D");
+  mytree->Branch("vertex_pitchangle_deg",&pangle,"vertex_pitchangle/D");
+  for (int i=0;i<nantenna;++i) {
+    brname = "pure_" + std::to_string(i) + "_V"; // unit in name
+    mytree->Branch(brname.data(), &purewave.at(i)); // point to vec_t dummy address
+  }
+  // hit data
+  mytree->Branch("hit_eventID", &hitevID); // point to vec<int>* dummy address
+  mytree->Branch("hit_trackID", &hittrID); // point to vec<int>* dummy address
+  mytree->Branch("hit_locx_m", &hitx); // point to vec<double>* dummy address
+  mytree->Branch("hit_locy_m", &hity); // point to vec<double>* dummy address
+  mytree->Branch("hit_locz_m", &hitz); // point to vec<double>* dummy address
+  mytree->Branch("hit_time_ns", &hittime); // point to vec<double>* dummy address
+  mytree->Branch("hit_edep_eV", &hitedep); // point to vec<double>* dummy address
+  mytree->Branch("hit_posttheta_deg", &hitposttheta); // point to vec<double>* dummy address
+
+}
+
+
+void WriterHitDigiToRoot::operator()(DataPack dp)
+{
+  // extract from datapack and assign to output branch variables with the correct address
+  nant    = dp.getTruthRef().nantenna;
+  mytree->SetBranchAddress("truth_nantenna",&nant);
+  samplingtime = dp.getTruthRef().sampling_time.numerical_value_in(s); // from quantity<ns> no unit for output
+  mytree->SetBranchAddress("truth_samplingtime_s",&samplingtime);
+  avomega      = dp.getTruthRef().average_omega.numerical_value_in(Hz); // quantity<Hz>
+  mytree->SetBranchAddress("truth_avomega_Hz",&avomega);
+  beatf        = dp.getTruthRef().beat_frequency.numerical_value_in(Hz); // quantity<Hz>
+  mytree->SetBranchAddress("truth_beatf_Hz",&beatf);
+  chirprate    = dp.getTruthRef().chirp_rate.numerical_value_in(Hz/s); // quantity<Hz>
+  mytree->SetBranchAddress("truth_chirp_Hz_s",&chirprate);
+  bfield       = dp.getTruthRef().bfield.numerical_value_in(T); // quantity<Hz>
+  mytree->SetBranchAddress("truth_bfield_T",&bfield);
+  // vertex
+  evID = dp.getTruthRef().vertex.eventID;
+  mytree->SetBranchAddress("vertex_evID",&evID);
+  trID = dp.getTruthRef().vertex.trackID;
+  mytree->SetBranchAddress("vertex_trackID",&trID);
+  posx    = dp.getTruthRef().vertex.posx.numerical_value_in(m); // quantity<m>
+  mytree->SetBranchAddress("vertex_posx_m",&posx);
+  posy    = dp.getTruthRef().vertex.posy.numerical_value_in(m); // quantity<m>
+  mytree->SetBranchAddress("vertex_posy_m",&posy);
+  posz    = dp.getTruthRef().vertex.posz.numerical_value_in(m); // quantity<m>
+  mytree->SetBranchAddress("vertex_posz_m",&posz);
+  kEnergy = dp.getTruthRef().vertex.kineticenergy.numerical_value_in(eV); // quantity<eV>
+  mytree->SetBranchAddress("vertex_kinenergy_eV",&kEnergy);
+  pangle  = dp.getTruthRef().vertex.pitchangle.numerical_value_in(deg); // quantity<deg>
+  mytree->SetBranchAddress("vertex_pitchangle_deg",&pangle);
+
+  std::string brname;
+  for (int i=0;i<nantenna;++i) {
+    purewave.at(i) = &dp.getTruthRef().pure.at(i); // vec_t*, copy
+    // store
+    brname = "pure_" + std::to_string(i) + "_V"; // unit in name
+    mytree->SetBranchAddress(brname.data(), &purewave.at(i));
+  }
+  if (! dp.hitsRef().empty()) { // there are hits to store
+    // extract hit data
+    for (hit_t hit : dp.hitsRef()) { // get hit struct from vector<hit_t>
+      hitevID->push_back(hit.eventID);
+      hittrID->push_back(hit.trackID);
+      hitx->push_back(hit.locx.numerical_value_in(m)); // no unit in root file
+      hity->push_back(hit.locy.numerical_value_in(m));
+      hitz->push_back(hit.locz.numerical_value_in(m));
+      hitedep->push_back(hit.edeposit.numerical_value_in(eV));
+      hittime->push_back(hit.timestamp.numerical_value_in(ns));
+      hitposttheta->push_back(hit.anglepost.numerical_value_in(deg));
+    }
+    mytree->SetBranchAddress("hit_eventID", &hitevID); // point to vec<int>* real address
+    mytree->SetBranchAddress("hit_trackID", &hittrID); // point to vec<int>* real address
+    mytree->SetBranchAddress("hit_locx_m", &hitx); // point to vec<double>* real address
+    mytree->SetBranchAddress("hit_locy_m", &hity); // point to vec<double>* real address
+    mytree->SetBranchAddress("hit_locz_m", &hitz); // point to vec<double>* real address
+    mytree->SetBranchAddress("hit_time_ns", &hittime); // point to vec<double>* real address
+    mytree->SetBranchAddress("hit_edep_eV", &hitedep); // point to vec<double>* real address
+    mytree->SetBranchAddress("hit_posttheta_deg", &hitposttheta); // point to vec<double>* real address
+  }
+  // all output collected, write it
+  mytree->Fill();
+  // clear internal
+  hitevID->clear();
+  hittrID->clear();
+  hitx->clear();
+  hity->clear();
+  hitz->clear();
+  hittime->clear();
+  hitedep->clear();
+  hitposttheta->clear();
+}
