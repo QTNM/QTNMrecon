4900ad0596b162cdd01c772e545144019ff931d5
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2026-08-10 15:57:49 +0100

    [examples] add python plotting script for new example.

--- examples/csvmfplot.py
+++ examples/csvmfplot.py
@@ -1,6 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("mf.csv")
-plt.plot(data)
+data = np.loadtxt("mf.csv",delimiter=',')
+plt.plot(data[:,1])
+plt.xlabel('sample points')
+plt.ylabel('filter output')
 plt.show()
+plt.plot(data[:,0],data[:,1])
+plt.xlabel('time lag [s]')
+plt.ylabel('filter output')
+plt.show()
+data2 = np.loadtxt("mf2.csv",delimiter=',')
+plt.plot(data2[:,1])
+plt.xlabel('sample points')
+plt.ylabel('filter output')
+plt.show()
+plt.plot(data2[:,0],data2[:,1])
+plt.xlabel('time lag [s]')
+plt.ylabel('filter output')
+plt.show()
+
